77c111e6beb53603840a7f29cafaa7cce4dd6794
malak-y <145293320+malak-y@users.noreply.github.com>
2024-07-02 21:24:12 +0300

    the first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log('jj');
+let arr= ["malak","mohamed",1];
+console.log(typeof (arr[2]));
+console.log(typeof(30.3333));
+let obj={
+   name:"malak",
+   age:22
+};
+obj.name="hana";
+console.log(obj);
